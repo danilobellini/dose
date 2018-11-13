141f48322f7812b7d32e3d5f065d4473a11102a4
Danilo J. S. Bellini <danilo.bellini@gmail.com>
2018-11-13 18:00:47 -0200

    Bump v1.2.1

--- dose/__init__.py
+++ dose/__init__.py
@@ -2,7 +2,7 @@
 
 # Be careful: this file is imported by setup.py!
 
-__version__ = "1.2.1.dev"
+__version__ = "1.2.1"
 __author__ = "Danilo de Jesus da Silva Bellini"
 __author_email__  = "danilo.bellini@gmail.com"
 __url__ = "https://github.com/danilobellini/dose"
